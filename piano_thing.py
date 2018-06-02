44444498f5c1498c22569da655b17ec07d6f8e91
Sophia Kovaleva <sophia.kovaleva@west.cmu.edu>
2018-06-02 15:41:35 +0300

    now drawing the keyboard

--- piano_thing.py
+++ piano_thing.py
@@ -6,11 +6,11 @@ import sys
 
 NOTES = ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"]
 OCTAVES = [1, 2, 3, 4, 5, 6, 7]
-ALL_NOTES = [n + str(o) for n in NOTES for o in OCTAVES]
-ALL_NOTES = ["A0", "A#0", "B"] + ALL_NOTES + ["C8"]
+ALL_NOTES = [n + str(o) for o in OCTAVES for n in NOTES ]
+ALL_NOTES = ["A0", "A#0", "B0"] + ALL_NOTES + ["C8"]
 
 class PianoThing(object):
-	def __init__(self, sample_path="my_samples", init_mixer=True, standalone=True):
+	def __init__(self, screen_size, sample_path="my_samples", init_mixer=True, standalone=True):
 		self.sample_path = sample_path
 		self.load_samples()
 		self.standalone = standalone
@@ -20,6 +20,61 @@ class PianoThing(object):
 			pygame.mixer.init(frequency=22050, size=-16, channels=2, buffer=512)
 			pygame.mixer.set_num_channels(88)
 		self.channels = {ALL_NOTES[i]: pygame.mixer.Channel(i) for i in xrange(88)}
+		self.screen_size = screen_size
+		self.white_key_size = 30, 120
+		self.black_key_size = 15, 80
+		self.draw_white_notes()
+		self.draw_black_notes()
+		self.playing_notes = set()
+
+
+	def render(self, screen):
+		screen.blit(self.white_notes_surface, (0,0))
+		screen.blit(self.black_notes_surface, (0,0))
+
+	def draw_white_notes(self, top_offset = 100, left_offset = 50):
+		self.white_notes_surface = pygame.Surface(self.screen_size)
+		self.white_notes_surface.fill((0,255,0))
+		self.white_notes_surface.set_colorkey((0,255,0))
+		cur_key = 0
+		self.white_note_rects = dict()
+		for note in ALL_NOTES:
+			if "#" in note:
+				continue
+			rect = self.draw_white_key(self.white_notes_surface, self.white_key_size, top_offset, left_offset+self.white_key_size[0]*cur_key)
+			cur_key += 1
+			self.white_note_rects[note] = rect
+
+
+	def draw_black_notes(self, top_offset = 100, left_offset = 50):
+		self.black_notes_surface = pygame.Surface(self.screen_size)
+		self.black_notes_surface.fill((0,255,0))
+		self.black_notes_surface.set_colorkey((0,255,0))
+		cur_note = 0
+		cur_black_key = 0
+		self.black_note_rects = dict()
+		cur_offset = left_offset
+		for note in ALL_NOTES:
+			if "#" in note:
+				rect = self.draw_black_key(self.black_notes_surface, self.black_key_size, top_offset, cur_offset - self.black_key_size[0]/2)
+				self.black_note_rects[note] = rect
+				cur_black_key += 1
+			else:
+				cur_offset += self.white_key_size[0]
+			cur_note += 1
+
+
+
+	def draw_black_key(self, surface, key_size, top, left):
+		rect = pygame.Rect((left, top), key_size)
+		pygame.draw.rect(surface, (0,0,0), rect, 0)
+		return rect
+
+	def draw_white_key(self, surface, key_size, top, left):
+		rect = pygame.Rect((left, top), key_size)
+		pygame.draw.rect(surface, (255,255,255), rect, 0)
+		pygame.draw.rect(surface, (0,0,0), rect, 2)
+		return rect
 
 	def init_standalone(self):
 		self.standalone_key_mapping = {pygame.K_a: "A", pygame.K_b: "B", pygame.K_c: "C",
@@ -45,30 +100,42 @@ class PianoThing(object):
 	def stop_all_notes(self):
 		for channel in self.channels.values():
 			channel.stop()
+
+	def add_to_playing(self, note, octave):
+		n_o = str(note) + str(octave)
+		self.playing_notes.add(n_o)
+		self.play_note(note, octave)
+
+	def remove_from_playing(self, note, octave):
+		n_o = str(note) + str(octave)
+		if n_o in self.playing_notes:
+			self.playing_notes.remove(n_o)
+		self.stop_note(note, octave)
 	
 	def stop_all_notes_not_held(self):
 		keys = pygame.key.get_pressed()
 		for note in ALL_NOTES:
+			only_note = note[:-1]
+			octave = int(note[-1])
 			if "4" in note and "#" not in note:
-				only_note = note[:-1]
 				key = self.standalone_inverse_key_mapping[only_note]
 				if keys[key]:
 					pass
 				else:
-					self.channels[note].stop()
+					self.remove_from_playing(only_note, octave)
 			else:
-				self.channels[note].stop()
+				self.remove_from_playing(only_note, octave)
 
 
 	def process_event_standalone(self, event):	
 		if event.type == pygame.KEYDOWN:
 			if event.key in self.standalone_key_mapping:
-				self.play_note(self.standalone_key_mapping[event.key], 4)
+				self.add_to_playing(self.standalone_key_mapping[event.key], 4)
 		elif event.type == pygame.KEYUP:
 			keys = pygame.key.get_pressed()
 			if not keys[pygame.K_SPACE]:
 				if event.key in self.standalone_key_mapping:
-					self.stop_note(self.standalone_key_mapping[event.key], 4)
+					self.remove_from_playing(self.standalone_key_mapping[event.key], 4)
 			if event.key == pygame.K_SPACE:
 				self.stop_all_notes_not_held()
 
@@ -99,14 +166,15 @@ def global_step_to_note_and_octave(global_step):
 
 
 def main():
-	size = (800, 600)
+	size = (1700, 600)
 	pygame.init()
 	
 	screen = pygame.display.set_mode(size, pygame.DOUBLEBUF | pygame.HWSURFACE)
 	pygame.display.set_caption(u"Piano thing")
-	pt = PianoThing()
+	pt = PianoThing(screen_size = size)
 
-
+	screen.fill((255,255,255))
+	screen.set_colorkey((0,255,0))
 	
 	while True:
 		for event in pygame.event.get():
@@ -114,6 +182,7 @@ def main():
 				sys.exit(0)
 			pt.process_event_standalone(event)
 
+		pt.render(screen)
 		pygame.display.flip()
 	
 
